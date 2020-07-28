8c30a07f2686a9681bb99b648ee018aa3e6b51c7
Brett Derrington <brett.derrington@gmail.com>
2020-07-28 15:08:21 -0500

    Base webpage functionality, CRUD is next; after is the logic for recommendations

--- src/index.js
+++ src/index.js
@@ -14,4 +14,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+//I'll definitely do this to help me learn about react more
 serviceWorker.unregister();
